8bafa7787c243b336797b02231d3aa421210530a
katecoderacademy <cam022111@coderacademy.edu.au>
2022-01-24 00:22:12 +1100

    modified blogpost

--- src/routes/blogpost.jsx
+++ src/routes/blogpost.jsx
@@ -2,9 +2,11 @@
 import React,{useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+
+
 export default function Blogpost() {
 let params = useParams();
-    
+
     
     return (
       <main style={{ padding: "1rem 0" }}>
