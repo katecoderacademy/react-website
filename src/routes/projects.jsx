c4136ee24ef529d7ecd8d2e8f39e23ad98e38f2f
katecoderacademy <cam022111@coderacademy.edu.au>
2022-01-24 00:34:55 +1100

    projects page now links out to projects, uses JSON

--- src/routes/projects.jsx
+++ src/routes/projects.jsx
@@ -1,7 +1,57 @@
-export default function Project() {
-    return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Project</h2>
+
+import React,{useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+
+export default function Blog() {
+
+  const [data,setData]=useState([]);
+  const getData=()=>{
+    fetch('data2.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        setData(myJson)
+      });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
+ 
+  // Style here
+      return (
+        <main style={{ padding: "1rem 0" }}>
+          <h2>Projects</h2>
+            
+            
+            <div className="project-container">
+                  {data.map((item)=>
+                  <><p><h3>{item.name}</h3></p>
+                      <p />{item.description}
+                      <p /><img src={item.imageURL} alt={item.title} width="60px" />
+                    
+                      <a href={item.link}
+                        style={{ display: "block", margin: "1rem 0" }}
+                        >Click here</a></>
+
+                        )}
+
+                      </div>
       </main>
     );
-  }
+}
+
+
+
+
+
+
